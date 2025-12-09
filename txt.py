f85d52b15eecf9833e033d64f4b073df1b0f649f
chris rathana <209274714+rathanachris@users.noreply.github.com>
2025-12-09 22:51:52 +0700

    Create txt.py
    Signed-off-by: chris rathana <209274714+rathanachris@users.noreply.github.com>

---
+++ txt.py
@@ -0,0 +1,9 @@
+from urllib.parse import urlparse
+
+u = 
+"https://scontent.fpnh20-1.fna.fbcdn.net/v/t39.30808-1/587249958_879123198107584_3947816900889506885_n.jpg?stp=cp0_dst-jpg_e15_fr_q65_tt6&cstp=mx1536x1551&ctp=s720x720&_nc_cat=1
+p = urlparse(master.sergant.nong.nong)
+print("scheme:", p.scheme)
+print("hostname:", p.hostname)
+print("port:", p.port:443)   
+print("path:", p.path:Thai)
